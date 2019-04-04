55eb90336db3f36aabeb8006cc63a9fad4f0bec0
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-04 17:56:54 +0900

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,43 +1,31 @@
 #include<stdio.h>
 
-void fizz(int i)			/*Fizzを出力*/
-{
-	if (i % 3 == 0)
-	{
+void fizz(int i){			/*Fizzを出力*/
+	if (i % 3 == 0){
 		printf("Fizz\n");
 	}
 }
 
-void buzz(int i)			/*Buzzを出力*/
-{
-	if (i % 5 == 0)
-	{
+void buzz(int i){			/*Buzzを出力*/
+	if (i % 5 == 0){
 		printf("Buzz\n");
 	}
 }
 
-void fizzbuzz(int i)		/*FizzBuzzを出力*/
-{
-	if (i % 15 == 0)
-	{
+void fizzbuzz(int i){		/*FizzBuzzを出力*/
+	if (i % 15 == 0){
 		printf("FizzBuzz\n");
 	}
 }
 
-void number(int i)			/*数字を表示*/
-{
-	if (i % 3 != 0 && i % 5 != 0 && i % 15 != 0)
-	{
+void number(int i){			/*数字を表示*/
+	if (i % 3 != 0 && i % 5 != 0){
 		printf("%d\n", i);
 	}
 }
 
-int main(void)
-{
-	int i;
-
-	for (i=1 ;i <= 100;i++)
-	{
+int main(void){
+	for (int i=1 ;i <= 100;i++){
 		fizzbuzz(i);
 		fizz(i);
 		buzz(i);
